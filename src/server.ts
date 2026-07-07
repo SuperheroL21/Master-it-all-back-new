d0cbda10b46ce9ee07077579328f58189c7b3e1d
txt <txt@MacBookPro.attlocal.net>
2026-07-07 01:12:38 -0700

    69

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import cors from "cors";
 import express, { Application } from "express";
 import routes from "./routes";
-// import { connectToMongo } from "./mongodb";
+ import { connectToMongo } from "./mongodb";
 
 const app: Application = express();
 const port = process.env.PORT ?? 3000;
@@ -14,23 +14,6 @@ app.use(express.json());
 
 
 app.use("/api", routes);
-
-// MongoDB startup boilerplate (uncomment when ready)
-//
-// const startServer = async (): Promise<void> => {
-//   await connectToMongo();
-//
-//   app.listen(port, () => {
-//     console.log(`Server is running on http://localhost:${port}`);
-//   });
-// };
-//
-// startServer().catch((error) => {
-//   console.error("Failed to start server:", error);
-//   process.exit(1);
-// });
-
-import { connectToMongo } from "./mongodb";
 
 const startServer = async (): Promise<void> => {
   await connectToMongo();
